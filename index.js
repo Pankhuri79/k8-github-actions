eccecdb5af1de7e9a2a92b9fecc389c7ad780faa
PANKHURI KUMARI <89061186+Pankhuri79@users.noreply.github.com>
2023-06-28 22:58:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,40 @@ const app = express();
 const port = 8080;
 
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+  const message = `
+    <html>
+      <head>
+        <title>Fancy Hello</title>
+        <style>
+          body {
+            font-family: Arial, sans-serif;
+            background-color: #f7f7f7;
+            color: #333;
+            text-align: center;
+          }
+          h1 {
+            font-size: 48px;
+            color: #ff8c00;
+            margin-top: 100px;
+          }
+          p {
+            font-size: 24px;
+            margin-top: 30px;
+          }
+        </style>
+      </head>
+      <body>
+        <h1>Welcome to the Fancy Hello App!</h1>
+        <p>╭━━━╮╱╱╱╱╭╮╱╭┳╮╱╭╮</p>
+        <p>╰╮╭╮┣╮╭┳╮╱┃┃╱┃┃┃╱┃┃</p>
+        <p>╱┃┃┃┃┃┃┃┃╱┃┃╱┃┃┃╱┃┃</p>
+        <p>╱┃┃┃┃┃┃┃┃╱┃┃╱┃┃┃╱┃┃</p>
+        <p>╭╯╰╯┃╰╯┃╰╮┃╰┳╯┃╰┳╯</p>
+        <p>╰━━━┻━━┻━╯╰━┻━┻━╯</p>
+      </body>
+    </html>
+  `;
+  res.send(message);
 });
 
 app.listen(port, () => {
